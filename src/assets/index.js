afa4c76d95589e791337d0e95e4fe30c71b0e151
Shuchita_33 <shuchi2133@gmail.com>
2024-06-07 18:51:32 +0530

    Added assets to cloud storage and updated urls in index.js

---
+++ src/assets/index.js
@@ -0,0 +1,23 @@
+const banner="https://res.cloudinary.com/shuchita/image/upload/v1717762445/banner_g1hurx.jpg"
+const logo="https://res.cloudinary.com/shuchita/image/upload/v1717762445/logo_qzvat1.jpg"
+const hm = 'https://res.cloudinary.com/shuchita/image/upload/v1717763695/hm_ikgntw.png';
+const sassafras = 'https://res.cloudinary.com/shuchita/image/upload/v1717763680/sss_s8jcju.png';
+const roadster = 'https://res.cloudinary.com/shuchita/image/upload/v1717763680/rdstr_mgpvfv.png';
+const levis = 'https://res.cloudinary.com/shuchita/image/upload/v1717763680/lvs_luceoc.png';
+const zara = 'https://res.cloudinary.com/shuchita/image/upload/v1717763680/zara_s79usy.jpg';
+const dolce = 'https://res.cloudinary.com/shuchita/image/upload/v1717763680/dg_o0of5e.png';
+const urbanic = 'https://res.cloudinary.com/shuchita/image/upload/v1717763680/ub_zndo3p.png';
+const tokyoTalkies = "https://res.cloudinary.com/shuchita/image/upload/v1717763698/tt_fooqul.png"
+
+const item1="https://res.cloudinary.com/shuchita/image/upload/v1717762446/Roadster_tshirt_209_qxuiz7.jpg"
+const item2="https://res.cloudinary.com/shuchita/image/upload/v1717762446/women_solid_pure_cotton_top_sassafaras_lyjhth.jpg"
+const item3="https://res.cloudinary.com/shuchita/image/upload/v1717762445/road._denim_skirt_699_mecoc7.jpg"
+const item4="https://res.cloudinary.com/shuchita/image/upload/v1717762445/her_by_invictus_309_xwokdc.jpg"
+const item5="https://res.cloudinary.com/shuchita/image/upload/v1717762445/Tokyo_Talkies_Peplum_Top_de1jnz.jpg"
+
+const col1="https://res.cloudinary.com/shuchita/image/upload/v1717764207/col1_tunhpm.jpg"
+const col2="https://res.cloudinary.com/shuchita/image/upload/v1717762445/ethnic_pqe8k8.jpg"
+const col3="https://res.cloudinary.com/shuchita/image/upload/v1717762444/bottoms_fiaixj.jpg"
+const col4="https://res.cloudinary.com/shuchita/image/upload/v1717762445/dresses_c5bkyr.jpg"
+
+export{logo,banner,hm,sassafras,roadster,levis,zara,dolce,urbanic,tokyoTalkies,item1,item2,item3,item4,item5,col1,col2,col3,col4}
